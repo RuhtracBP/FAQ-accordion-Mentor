875fc7403036041373b6b6657d50c7fc204a5748
Arthur <arthurhcbp@gmail.com>
2021-01-16 17:03:00 -0300

    pequenos ajustes no css mobile

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 
 
 const questions = document.querySelectorAll(".accordion-item-header") ;
+/* const box = document.querySelector(".box") ; */
+
+
 
 
 questions.forEach( question => {
     question.addEventListener("click", () => {
+
+        /* var isMobile = /Mobi/i.test(navigator.userAgent);
+            if (isMobile) {
+                box.classList.toggle("expand");
+                console.log(box.classList);
+
+            } */
+        
         if(question.parentNode.classList.contains("active")) {
             question.parentNode.classList.toggle("active");
+
         } else {
             questions.forEach( question => {
                 question.parentNode.classList.remove("active");
