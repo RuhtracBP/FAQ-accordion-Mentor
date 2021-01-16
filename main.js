ebf583b366dc39fd94efa567b6ec226f6f213f8b
Arthur <arthurhcbp@gmail.com>
2021-01-16 16:05:50 -0300

    projeto v1.0

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+const questions = document.querySelectorAll(".accordion-item-header") ;
+
+
+questions.forEach( question => {
+    question.addEventListener("click", () => {
+        if(question.parentNode.classList.contains("active")) {
+            question.parentNode.classList.toggle("active");
+        } else {
+            questions.forEach( question => {
+                question.parentNode.classList.remove("active");
+            });
+            question.parentNode.classList.add("active");
+        }
+    })
+})
